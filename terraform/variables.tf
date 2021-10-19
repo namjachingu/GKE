ac045864761a8138cbf44ef1e061afaf145448f0
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-10-19 17:23:13 +0200

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,45 @@
+variable "apikey" {
+  description = "Intersight API Key."
+  type    = string
+}
+
+variable "project" {
+  description = "The GCP Project ID."
+  type = string
+}
+
+variable "location" {
+  description = "The location (region or zone) to host the GKE cluster."
+  type        = string
+  default = "europe-west3-a"	
+}
+
+variable "region" {
+  description = "The region to host the GKE cluster in."
+  type = string
+}
+
+variable "appd_account_name" {
+  description = "Account name needed to log into AppDynamics Dashboard."
+  type = string
+  }
+
+variable "appd_global_account" {
+  description = "Global AppDynamics account. Found in AppDynamics Dashboard under Settings - License - Account."
+  type = string
+  }
+
+variable "appd_controller_key" {
+  description = "AppDynamics controller key. Found in AppDynamics Dashboard under Settings - License - Account."
+  type = string
+  }
+
+variable "appd_username" {
+  description = "Username needed to log into AppDynamics Dashboard."
+  type = string
+  }
+
+variable "appd_password" {
+  description = "Password needed to log into AppDynamics Dashboard."
+  type = string
+  }
